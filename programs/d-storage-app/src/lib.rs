40848ebfb0ff42203ab64734a4b1ff9f720ef7c5
lei liu <chan.liu1983@gmail.com>
2025-05-30 14:26:24 +0800

    Update

--- programs/d-storage-app/src/lib.rs
+++ programs/d-storage-app/src/lib.rs
@@ -1,7 +1,74 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Token, TokenAccount, Transfer};
 use std::collections::HashMap;
 
 declare_id!("Ed5i4GsQCTU5NLvgieHUWHFAGfBJ61NfktWw271fesEJ");
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = signer, space = 8 + 32 + 4 + 1000)] // 8 for discriminator, 32 for owner, 4 for vec len, 1000 for data
+    pub store: Account<'info, KeyValueStore>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Save<'info> {
+    #[account(mut)]
+    pub store: Account<'info, KeyValueStore>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Query<'info> {
+    pub store: Account<'info, KeyValueStore>,
+}
+
+#[derive(Accounts)]
+pub struct TransferOwnership<'info> {
+    #[account(mut)]
+    pub store: Account<'info, KeyValueStore>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+#[account]
+pub struct KeyValueStore {
+    pub owner: Pubkey,
+    pub data: Vec<u8>, // Serialized HashMap<String, String>
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("You are not authorized to perform this action")]
+    Unauthorized,
+}
+
+#[derive(Accounts)]
+pub struct TokenTransfer<'info> {
+    #[account(mut)]
+    pub from: Signer<'info>,
+    /// CHECK: This is the destination account that will receive the tokens
+    #[account(mut)]
+    pub to: AccountInfo<'info>,
+    pub token_program: Program<'info, Token>,
+    #[account(
+        mut,
+        constraint = from_ata.owner == from.key(),
+        constraint = from_ata.mint == mint.key()
+    )]
+    pub from_ata: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        constraint = to_ata.owner == to.key(),
+        constraint = to_ata.mint == mint.key()
+    )]
+    pub to_ata: Account<'info, TokenAccount>,
+    /// CHECK: This is the token mint
+    pub mint: AccountInfo<'info>,
+}
 
 #[program]
 pub mod d_storage_app {
@@ -82,46 +149,26 @@ pub mod d_storage_app {
         msg!("Store ownership transferred to: {}", new_owner);
         Ok(())
     }
-}
 
-#[derive(Accounts)]
-pub struct Initialize<'info> {
-    #[account(init, payer = signer, space = 8 + 32 + 4 + 1000)] // 8 for discriminator, 32 for owner, 4 for vec len, 1000 for data
-    pub store: Account<'info, KeyValueStore>,
-    #[account(mut)]
-    pub signer: Signer<'info>,
-    pub system_program: Program<'info, System>,
-}
+    pub fn transfer_token(
+        ctx: Context<TokenTransfer>,
+        amount: u64
+    ) -> Result<()> {
+        let transfer_instruction = Transfer {
+            from: ctx.accounts.from_ata.to_account_info(),
+            to: ctx.accounts.to_ata.to_account_info(),
+            authority: ctx.accounts.from.to_account_info(),
+        };
 
-#[derive(Accounts)]
-pub struct Save<'info> {
-    #[account(mut)]
-    pub store: Account<'info, KeyValueStore>,
-    #[account(mut)]
-    pub signer: Signer<'info>,
-}
+        token::transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                transfer_instruction,
+            ),
+            amount,
+        )?;
 
-#[derive(Accounts)]
-pub struct Query<'info> {
-    pub store: Account<'info, KeyValueStore>,
-}
-
-#[derive(Accounts)]
-pub struct TransferOwnership<'info> {
-    #[account(mut)]
-    pub store: Account<'info, KeyValueStore>,
-    #[account(mut)]
-    pub signer: Signer<'info>,
-}
-
-#[account]
-pub struct KeyValueStore {
-    pub owner: Pubkey,
-    pub data: Vec<u8>, // Serialized HashMap<String, String>
-}
-
-#[error_code]
-pub enum ErrorCode {
-    #[msg("You are not authorized to perform this action")]
-    Unauthorized,
+        msg!("Transferred {} IALT tokens", amount);
+        Ok(())
+    }
 }
